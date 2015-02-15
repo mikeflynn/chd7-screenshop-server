540e8f1214089cf93d7eae142f452ca426d37ba5
Mike Flynn <mfflynn@gmail.com>
2015-02-14 23:27:11 -0800

    Reworked code to allow for the return of the whole convesation with message direction metadata.

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var tesseract_bin string = "/usr/local/bin/tesseract"
+var tesseract_bin string = "tesseract"
+var convert_bin string = "convert"
 
 func respond(resp interface{}) string {
 	jsonStr, _ := json.Marshal(resp)
@@ -57,28 +58,52 @@ func isMessages(filename string) bool {
 	return false
 }
 
-func trimImage(filename string) bool {
-	cmd := exec.Command("/usr/local/bin/convert", filename,
-		"-crop", "+0+200", "-crop", "-200+-50", "+repage",
-		"-channel", "rgba", "-alpha", "set", "-fuzz", "40%", "-fill", "white", "-opaque", "#1D62F0",
-		filename)
+func convertImage(filename string, job string) (string, bool) {
+
+	hasError := false
+	new_filename := filename + "-" + job
+
+	var cmd *exec.Cmd
+
+	if job == "all" {
+		cmd = exec.Command(convert_bin, filename,
+			// Cut the top and bottom off
+			"-crop", "+0+200", "-crop", "+0+-60", "+repage",
+			"-channel", "rgba", "-alpha", "set", "-fuzz", "15%", "-fill", "white", "-opaque", "#999999",
+			new_filename)
+	} else if job == "in" {
+		cmd = exec.Command(convert_bin, filename,
+			// Focus on incoming text
+			"-crop", "+0+200", "-crop", "+0+-60", "+repage",
+			"-channel", "rgba", "-alpha", "set", "-fuzz", "40%", "-fill", "white", "-opaque", "#1D62F0", "-opaque", "#0BD318",
+			new_filename)
+	} else if job == "out" {
+		cmd = exec.Command(convert_bin, filename,
+			// Focus on outgoing text
+			"-crop", "+0+200", "-crop", "+0+-60", "+repage",
+			"-channel", "rgba", "-alpha", "set", "-fuzz", "20%", "-fill", "black", "-opaque", "#DBDDDE", "-opaque", "#000000", "-opaque", "#ffffff", "-opaque", "#cbcbcb",
+			new_filename)
+	} else {
+		return "", true
+	}
+
 	err := cmd.Start()
 	if err != nil {
 		panic(err)
-		return false
+		hasError = true
 	}
 
 	err = cmd.Wait()
 	if err != nil {
 		panic(err)
-		return false
+		hasError = true
 	}
 
-	return true
+	return new_filename, hasError
 }
 
-func tesseract(filename string) []string {
-	outfile := "/tmp/" + md5hash(filename) + ".tesseract"
+func tesseract(filename string, lineDelimiter string) []string {
+	outfile := filename + ".tesseract"
 
 	cmd := exec.Command(tesseract_bin, filename, outfile)
 	err := cmd.Start()
@@ -94,7 +119,7 @@ func tesseract(filename string) []string {
 		return nil
 	}
 
-	return regexp.MustCompile("\n\n").Split(string(dat), -1)
+	return regexp.MustCompile(lineDelimiter).Split(string(dat), -1)
 }
 
 // Handlers
@@ -108,17 +133,21 @@ type ScanResponse struct {
 	Error    bool
 	ErrorMsg string
 	TmpFile  string
-	Lines    []string
+	Lines    []map[string]string
+}
+
+func ErrorResp(w http.ResponseWriter, msg string) {
+	resp := new(ScanResponse)
+	resp.Error = true
+	resp.ErrorMsg = msg
+	fmt.Fprintf(w, respond(resp))
 }
 
 func ScanHandler(w http.ResponseWriter, r *http.Request) {
 	file, header, err := r.FormFile("file")
 
 	if err != nil {
-		resp := new(ScanResponse)
-		resp.Error = true
-		resp.ErrorMsg = "Missing file upload."
-		fmt.Fprintf(w, respond(resp))
+		ErrorResp(w, "Missing file upload.")
 		return
 	}
 
@@ -128,10 +157,7 @@ func ScanHandler(w http.ResponseWriter, r *http.Request) {
 
 	out, err := os.Create(tmpFilename)
 	if err != nil {
-		resp := new(ScanResponse)
-		resp.Error = true
-		resp.ErrorMsg = "Can't create temp file."
-		fmt.Fprintf(w, respond(resp))
+		ErrorResp(w, "Can't create temp file.")
 		return
 	}
 
@@ -139,29 +165,77 @@ func ScanHandler(w http.ResponseWriter, r *http.Request) {
 
 	_, err = io.Copy(out, file)
 	if err != nil {
-		resp := new(ScanResponse)
-		resp.Error = true
-		resp.ErrorMsg = "Can't copy to temp file."
-		fmt.Fprintf(w, respond(resp))
+		ErrorResp(w, "Can't copy to temp file.")
 		return
 	}
 
 	if isMessages(tmpFilename) == false {
-		resp := new(ScanResponse)
-		resp.Error = true
-		resp.ErrorMsg = "Not a text message screenshot."
-		fmt.Fprintf(w, respond(resp))
+		ErrorResp(w, "Not a text message screenshot.")
 		return
 	}
 
-	trimImage(tmpFilename)
+	// Prep and OCR full image
+	trimmedFile, hasError := convertImage(tmpFilename, "all")
+	if hasError {
+		ErrorResp(w, "Failed to trim image.")
+		return
+	}
 
-	lines := tesseract(tmpFilename)
+	allLines := tesseract(trimmedFile, "\n")
+
+	// Prep and OCR incoming
+	incomingFile, hasError := convertImage(tmpFilename, "in")
+	if hasError {
+		ErrorResp(w, "Image optimization failed (in).")
+		return
+	}
+
+	inLines := tesseract(incomingFile, "\n")
+
+	// Prep and OCR outgoing
+	outgoingFile, hasError := convertImage(tmpFilename, "out")
+	if hasError {
+		ErrorResp(w, "Image optimization failed (out).")
+		return
+	}
+
+	outLines := tesseract(outgoingFile, "\n")
+
+	returnLines := make([]map[string]string, len(allLines))
+
+	for i := range allLines {
+		if strings.TrimSpace(allLines[i]) == "" {
+			continue
+		}
+
+		lineType := "unknown"
+
+		for k := range inLines {
+			if strings.TrimSpace(inLines[k]) == strings.TrimSpace(allLines[i]) {
+				lineType = "incoming"
+				break
+			}
+		}
+
+		if lineType == "unknown" {
+			for k := range outLines {
+				if strings.TrimSpace(outLines[k]) == strings.TrimSpace(allLines[i]) {
+					lineType = "outgoing"
+					break
+				}
+			}
+		}
+
+		returnLines[i] = map[string]string{
+			"type": lineType,
+			"text": allLines[i],
+		}
+	}
 
 	resp := new(ScanResponse)
 	resp.Error = false
 	resp.TmpFile = header.Filename
-	resp.Lines = lines
+	resp.Lines = returnLines
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, respond(resp))
